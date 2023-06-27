33f2f8b151f4b582c588dd030cb688d63e8ae86f
SzMartin1 <martinszabo775@gmail.com>
2023-06-27 22:49:18 +0200

    Added foodpage and cartpage

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { StarRatingComponent } from './components/partials/star-rating/star-rating.component';
 import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
+import { FoodPageComponent } from './components/pages/food-page/food-page.component';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { TagsComponent } from './components/partials/tags/tags.component';
     HomeComponent,
     StarRatingComponent,
     SearchComponent,
-    TagsComponent
+    TagsComponent,
+    FoodPageComponent,
+    CartPageComponent,
+    TitleComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
